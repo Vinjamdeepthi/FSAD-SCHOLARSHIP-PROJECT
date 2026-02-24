85dac5d9455b9839a8d64ff66e858d8c08dc49d2
Vinjam Deepthi <2400033271@kluniversity.in>
2026-02-24 13:58:38 +0530

    add

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
